463bfc7ffaa948de83ce3be01f2f2504fb47fe09
유승훈 <yuseunghun@yuseunghun-ui-MacBookPro.local>
2022-06-29 01:44:47 +0900

    ajax prac

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}))
-
 const MongoClient = require('mongodb').MongoClient
-MongoClient.connect('mongodb+srv://yungs0917:seung8869@@cluster0.gszmo.mongodb.net/?retryWrites=true&w=majority',function(에러,client){
+app.set('view engine','ejs');
+
+var db;
+MongoClient.connect('mongodb+srv://yungs0917:seung8869%40@cluster0.gszmo.mongodb.net/?retryWrites=true&w=majority',function(에러,client){
+
+    if(에러){
+        return console.log(에러)
+    }
+
+    db = client.db('todoapp');
+    db.collection('post').insertOne( { 이름 : 'john',나이 : '20', _id:100},function(에러,결과){
+        console.log('저장완료');
+    });
+
     app.listen(8080,function(){
         console.log('listenening on 8080');
     });
@@ -24,6 +36,25 @@ app.get('/write',function(요청,응답){
 });
 
 app.post('/add',function(요청,응답){
+    db.collection('counter').findOne({name : '게시물갯수'}, (에러,결과) =>{
+        console.log(결과.totalPost);
+        var 총게시물갯수 = 결과.totalPost
+        db.collection('post').insertOne({_id: 총게시물갯수 + 1 , 이메일 : 요청.body.title , 세부사항 : 요청.body.content },(에러,결과) =>{
+            console.log('이메일과 세부사항 저장완료');
+            db.collection('counter').updateOne({name :'게시물갯수'},{ $inc : {totalPost:1}},function(에러,결과){
+                if(에러){return console.log(에러)}
+            })
+        } )
+        
+    })
     console.log(요청.body)
+    
     응답.send('전송완료')
 });
+
+app.get('/list', function(요청, 응답){
+    db.collection('post').find().toArray(function(에러, 결과){
+      console.log(결과)
+      응답.render('list.ejs', { posts : 결과 })
+    })
+  })
