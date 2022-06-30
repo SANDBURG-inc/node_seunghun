82b4afe1f6b10353f791ae61bcb058c856eee4f7
유승훈 <yuseunghun@yuseunghun-ui-MacBookPro.local>
2022-06-30 13:49:25 +0900

    delete

--- server.js
+++ server.js
@@ -58,3 +58,12 @@ app.get('/list', function(요청, 응답){
       응답.render('list.ejs', { posts : 결과 })
     })
   })
+
+app.delete('/delete',function(요청,응답){
+    console.log(요청.body)
+    요청.body._id = parseInt(요청.body._id);
+    db.collection('post').deleteOne(요청.body,function(에러,결과){
+        console.log('삭제완료');
+        응답.status(200).send({ message : '성공했습니다'});
+    })
+})
